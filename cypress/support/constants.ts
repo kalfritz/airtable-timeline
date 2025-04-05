ecbd8743eba111903a661a3adc663dd4013ffa14
Luiz Ramos <luizeliasceem@gmail.com>
2025-04-04 22:52:26 -0300

    clean up duplicated file

--- cypress/support/constants.ts
+++
@@ -1,7 +0,0 @@
-export const TEST_IDS = {
-  timelineItem: 'timeline-item',
-  modalInput: 'modal-input',
-  modalSave: 'modal-save',
-  modalCancel: 'modal-cancel',
-  tooltip: 'timeline-tooltip'
-} as const; 
